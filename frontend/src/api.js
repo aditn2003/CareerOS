c8640d9b9b4df52513e76b0420f01d84527c7d9f
Adit Nuwal <144288634+aditn2003@users.noreply.github.com>
2025-12-15 12:43:38 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const baseURL =
   import.meta.env.VITE_API_URL ||
   (window.location.hostname === "localhost"
-    ? "http://localhost:4000"
-    : "http://backend:4000");
+    ? "http://localhost:4000");
 
 export const api = axios.create({
   baseURL,
